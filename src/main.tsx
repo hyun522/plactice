736dd20b8354d4ddbb4034b12d9cf7c85e792d1e
hyun522 <jhj1004v@naver.com>
2024-11-04 23:23:19 +0900

    FEAT : 드롭다운 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
+// src/index.tsx
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'mobx-react';
+import stores from './stores'; // MobX 스토어들
+import App from './App';
 import './index.css';
-import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider {...stores}>
+      <App />
+    </Provider>
   </StrictMode>
 );
