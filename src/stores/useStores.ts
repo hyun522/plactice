9cf4643a6649b92bd862605b7c3424c813e148ef
hyun522-admin <hjgo7878@gmail.com>
2024-11-08 10:51:51 +0900

    Revert "FEAT : 드롭다운 구현"
    This reverts commit 736dd20b8354d4ddbb4034b12d9cf7c85e792d1e.

--- src/stores/useStores.ts
+++
@@ -1,9 +0,0 @@
-// src/stores/useStores.js
-import React from 'react';
-import { MobXProviderContext } from 'mobx-react';
-
-function useStores() {
-  return React.useContext(MobXProviderContext);
-}
-
-export default useStores;
