7bf84ab82d031e094814bdaafa8e78550a3bbbfe
DESKTOP-FLI861G\nstri <jhj1004v@naver.com>
2025-02-28 14:21:27 +0900

    carousel, svg 색상변경 작업

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,15 @@ import EllipseIcon from "../components/Ellipse";
 export default function HomePage() {
   return (
     <>
-      <div>
-        <p>안녕하세요 홈페이지입니다:)</p>
+      <div className="content-container">
+        <div>
+          <p>안녕하세요 홈페이지입니다:)</p>
+        </div>
+        <div style={{ backgroundColor: "skyblue" }}>
+          <Carousel />
+        </div>
+        <EllipseIcon fill="green" />
       </div>
-      <div style={{ backgroundColor: "skyblue" }}>
-        <Carousel />
-      </div>
-      <EllipseIcon fill="green" />
     </>
   );
 }
